e1ecdfee30a4081afee130fd3330db94a07c6b39
johannes000 <8878597+johannes000@users.noreply.github.com>
2025-09-05 02:13:13 +0200

    Add SDL_ttf package and format options in CPMPackages.cmake

--- cmake/CPMPackages.cmake
+++ cmake/CPMPackages.cmake
@@ -3,7 +3,19 @@ CPMAddPackage(
   NAME SDL
   GITHUB_REPOSITORY libsdl-org/SDL
   GIT_TAG release-3.2.8
-  OPTIONS "SDL_SHARED OFF" "SDL_STATIC ON"
+  OPTIONS 
+    "SDL_SHARED OFF" 
+    "SDL_STATIC ON"
+    "SDL_TESTS OFF"
+)
+
+CPMAddPackage(
+  NAME SDL_ttf
+  GITHUB_REPOSITORY libsdl-org/SDL_ttf
+  GIT_TAG release-3.2.2
+  OPTIONS 
+    "SDLTTF_VENDORED ON"
+    "SDLTTF_HARFBUZZ OFF"
 )
 
 CPMAddPackage(
